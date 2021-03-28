a68cd04d04ac13b8583e6fd2830d85a44c368828
mnemch <nemchinova.marya@gmail.com>
2021-03-28 16:37:23 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        weather
+        weather <a href="https://github.com/mnemch/react_app" target="_blank"> open github</a>
       </header>
+      </div>
     </div>
   );
 }
