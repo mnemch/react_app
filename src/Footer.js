c37f76c1c21369b075e49907a8579f5cb96695b2
mnemch <nemchinova.marya@gmail.com>
2021-03-29 02:19:46 +0200

    rel="noreferrer"

--- src/Footer.js
+++ src/Footer.js
@@ -4,22 +4,22 @@ export default function Footer() {
   return (
     <div className="footer">
     <div className="hellohi">
-        <a href="https://github.com/mnemch/weather-app/tree/a05d51c3a921451f012f47579c9baec700f1410e" target="_blank">
+        <a href="https://github.com/mnemch/react_app" target="_blank" rel="noreferrer">
             Open-source code {""}
         </a>
         by Mariia Nemchinova
 
-        <a href="https://www.shecodes.io/users/8831/" target="_blank">👩‍💻 </a>
-        <a href="https://www.linkedin.com/in/mariia-nemchinova-693609110/" target="_blank">
+        <a href="https://www.shecodes.io/users/8831/" target="_blank" rel="noreferrer">👩‍💻 </a>
+        <a href="https://www.linkedin.com/in/mariia-nemchinova-693609110/" target="_blank" rel="noreferrer">
             <i className="fab fa-linkedin">ln</i>
         </a>
-        <a href="https://twitter.com/Maria91259872" target="_blank">
+        <a href="https://twitter.com/Maria91259872" target="_blank" rel="noreferrer">
             <i className="fab fa-twitter"> tw</i>
         </a>
         <br />
         <div className="language">
             <div id="div1">
-                <a href="https://practical-johnson-7e7b99.netlify.app/" onclick="setLanguage('en')"> 🇬🇧English </a>
+                <a href="" onclick="setLanguage('en')"> 🇬🇧English </a>
                 <a href="#" onclick="setLanguage('es')"> 🇪🇸Español </a>
             </div>
             </div>
